4d4944e854adbeba93f43e274708e99d09f958ab
Nicola Rettich <ga68xul@mytum.de>
2015-10-28 10:47:23 +0100

    made a change

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -12,7 +12,13 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String name;
+		HelloUser user = new HelloUser();
 		System.out.println("Hello ASE2015 - how are you? :)");
+		name = user.getBenutzereingabe("Please enter your name: ");
+		System.out.println("Hello "+name+" !");
+		
+		
 	}
 
 }
